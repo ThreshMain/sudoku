3b6095b86f8d8e9a5a5e1618909853a007522cad
ThreshMain <martinkos007@gmail.com>
2021-01-16 01:12:31 +0100

    first commit

--- js/store.js
+++ js/store.js
@@ -1,27 +1,27 @@
-var Redux = require('redux');
-var Boards = require('./boards');
-var Sudoku = require('./sudoku');
-var cloneDeep = require('lodash.clonedeep');
+var Redux = require("redux");
+var Boards = require("./boards");
+var Sudoku = require("./sudoku");
+var cloneDeep = require("lodash.clonedeep");
 
-var Store = Redux.createStore(function(state, action) {
+var Store = Redux.createStore(function (state, action) {
   if (!state) {
     state = {};
   } else {
     state = cloneDeep(state);
   }
   switch (action.type) {
-    case 'RESUME_GAME':
+    case "RESUME_GAME":
       state.game = JSON.parse(localStorage.currentGame);
       state.game.time = new Date(state.game.time);
       break;
-    case 'NEW_GAME':
+    case "NEW_GAME":
       state.game = Sudoku.boardToGame(Boards.randomBoard(action.difficulty));
       break;
-    case 'CHANGE_VALUE':
+    case "CHANGE_VALUE":
       state.game.cells[action.i][action.j].value = action.value;
       break;
-    case 'ADD_SECOND':
-      if (state.game) {
+    case "ADD_SECOND":
+      if (state.game && !Sudoku.isComplete(state.game.cells)) {
         state.game.time.setSeconds(state.game.time.getSeconds() + 1);
       }
       break;
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,23 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
   entry: "./js/index.jsx",
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
   module: {
     loaders: [
       { test: /\.css$/, loader: "style!css" },
-      // tell webpack to use jsx-loader for all *.jsx files
-      // { test: /\.jsx$/, loader: 'jsx-loader?insertPragma=React.DOM&harmony' },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" }
-    ]
+      { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ["", ".js", ".jsx"],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.DedupePlugin()
-  ]
+    new webpack.optimize.DedupePlugin(),
+  ],
 };
